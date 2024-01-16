2b644cfe8840a8154724fd0e59b932df56d1c29f
nickiecdp <133632684+nickiecdp@users.noreply.github.com>
2024-01-16 16:39:50 -0500

    updated html and js for humidity wind and descr using api

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ function refreshWeather(response){
     temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
 
-    cityElement.innerHTML = response.data.city
-    temperatureElement.innerHTML = Math.round(temperature)
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
  
 }
 
