2c0ef61c84bd1906ffb9470abafbd8a9f35b6f6e
nickiecdp <133632684+nickiecdp@users.noreply.github.com>
2024-01-16 17:08:25 -0500

    added time from api. will format to make date clearer

--- src/app.js
+++ src/app.js
@@ -5,14 +5,19 @@ function refreshWeather(response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+     let timeElement = document.querySelector("#time");
+     let date = new Date(response.data.time * 1000);
+
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = 
  
 }
+
 
 function searchCity(city) {
   
