70fb2ef6d6922549e895b7fbf23162baddd85f64
nickiecdp <133632684+nickiecdp@users.noreply.github.com>
2024-01-21 14:25:29 -0500

    UPDATED ICON

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ function refreshWeather(response){
     let windElement = document.querySelector("#wind");
      let timeElement = document.querySelector("#time");
      let date = new Date(response.data.time * 1000);
-
-
+     let iconElement=document.querySelector("#icon")
+     
+    iconElement.innerHTML= `<img src=${response.data.condition.icon_url}
+                  class="weather-app-image" />`
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
